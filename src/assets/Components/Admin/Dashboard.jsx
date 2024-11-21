4ab84b944f8a457966613900a830ef94bff56cef
Shuhaib77 <shuhaibbasheer20022@gmail.com>
2024-11-21 13:29:14 +0530

    all compleated

--- src/assets/Components/Admin/Dashboard.jsx
+++ src/assets/Components/Admin/Dashboard.jsx
@@ -55,7 +55,7 @@ function Dashboard() {
     ordersss();
   }, []);
 
-  console.log(ausers.length);
+  console.log(ausers,"dccd");
   
 
   // Set up Chart.js
@@ -87,7 +87,7 @@ function Dashboard() {
 
   return (
     <>
-      <div className="flex justify-center m-10 w-100% mt-5">
+      <div className="flex justify-center m-10 w-[123vh] mt-5">
         {/* Dashboard cards */}
         <div className="bg-gradient-to-r from-teal-400 to-yellow-200 w-[35vh] h-[15vh] mr-4">
           <div className="ml-3 mt-5">
